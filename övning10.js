082468d59a0478fc3abe82d596bc64da409f933a
tartagliajk <jeonniekimvazae@gmail.com>
2022-09-15 21:25:46 +0200

    Update övning10.js
    Kom på i slutet att jag kunde lägga upp på github istället för att lämna in varje fil separat :D

--- övning10.js
+++ övning10.js
@@ -9,3 +9,16 @@ var ps = require("prompt-sync");
 var prompt = ps();
 console.log("Write how much electricity you have used");
 let electricityUsage = parseInt(prompt(">"));
+if(electricityUsage <= 50){
+    let price50Mark = electricityUsage * 0.5
+    console.log("You need to pay:", price50Mark,"kr");
+}else if(electricityUsage > 50 && electricityUsage <= 150){
+    let price150Mark = electricityUsage * 0.77
+    console.log("You need to pay:", price150Mark,"kr");
+}else if(electricityUsage > 150 && electricityUsage <= 250){
+    let price250Mark = electricityUsage * 1.3
+    console.log("You need to pay:",price250Mark,"kr");
+}else{
+    let priceOver250 = (electricityUsage * 1.7) * 1.2
+    console.log("You need to pay",priceOver250,"kr");
+};
