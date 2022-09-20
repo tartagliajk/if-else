30cb401e23a8642d5d32c90aecfdb8726872710f
tartagliajk <jeonniekimvazae@gmail.com>
2022-09-20 10:30:56 +0200

    Update övning7.js
    Glömde delen där koden skulle säga om det var en konstant eller en vokal, så jag la till det

--- övning7.js
+++ övning7.js
@@ -9,14 +9,27 @@ Annars skriver den “Ogiltig input”.
 */
 var ps = require("prompt-sync");
 var prompt = ps();
-let letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","å","ä","ö"];
-let specialCharacters = ["~","`","!","@","#","$","%","^","&","*","(",")","-","_","+","=","{","}","[","]","|","/",":",";",'"',"'","<",">",",",".","?","\\"]
+let lettersSmall = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","å","ä","ö"];
+let lettersBig = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","Å","Ä","Ö"];
+let vowelsSmall = ["a", "e", "i", "o", "u", "y", "å", "ä", "ö"];
+let vowelsBig = ["A","E","I","O","U","Y","Å","Ä","Ö"];
+let specialCharacters = ["~","`","!","@","#","$","%","^","&","*","(",")","-","_","+","=","{","}","[","]","|","/",":",";",'"',"'","<",">",",",".","?","\\"];
 console.log("Write your letter, number or special character");
 let object = prompt(">");
 if (parseInt(object)){
     console.log("number");
-}else if(letters.includes(object)){
-    console.log("letter");
+}else if(lettersBig.includes(object)){
+    if(vowelsBig.includes(object)){
+        console.log("Big letter and an vowel")
+    }else{
+        console.log("Big letter and a consonant")
+    }
+}else if(lettersSmall.includes(object)){
+    if(vowelsSmall.includes(object)){
+        console.log("Small letter and an vowel")
+    }else{
+        console.log("Small letter and a consonant")
+    }
 }else if(specialCharacters.includes(object)){
     console.log("special character");
 }else{
